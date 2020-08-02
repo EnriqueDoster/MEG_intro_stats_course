dbab5d30ee0d67be104cd2b8b6df7b4738a6d828
EnriqueDoster <12348747+EnriqueDoster@users.noreply.github.com>
2020-08-01 19:48:51 -0600

    Update Lesson3_Step4_Advanced_plotting.R

--- scripts/Lesson3_Step4_Advanced_plotting.R
+++ scripts/Lesson3_Step4_Advanced_plotting.R
@@ -3,7 +3,7 @@
 source("./scripts/load_data.R")
 
 
-######################
+#####################
 ##### Filter Data ####
 ######################
 
@@ -95,13 +95,12 @@ design_group = model.matrix(~0 + Group)
 # the fitZig function, but notice that we use the flag, useCSSoffset = FALSE so that it does not use
 # the normalization factor is not used and instead, we feed it the "zeroMod" which is
 # based on the raw counts.
-filtered_phylum_microbiome.metaseq <- cumNorm(filtered_phylum_microbiome.metaseq)
+cumNorm(filtered_phylum_microbiome.metaseq)
 
 zig_model <- fitZig(obj= filtered_phylum_microbiome.metaseq, mod = design_group, zeroMod=zero_mod, useCSSoffset = FALSE)
 
 # Use Ebayes to adjust model fit
 ebayes_zig_model <- eBayes(zig_model$fit)
-ebayes_zig_model
 
 zigFit_Group = zig_model$fit
 finalMod_Group = zig_model$fit$design
@@ -109,16 +108,68 @@ contrast_Group = makeContrasts(GroupControl-GroupTreatment, levels=finalMod_Grou
 zigFit_contrasts = contrasts.fit(zigFit_Group, contrast_Group)
 EB_zigFit_contrasts = eBayes(zigFit_contrasts)
 
-# make table of results, you can output these results with "write.csv()"
+# Make table of results, you can output these results with "write.csv()"
 # Explore this table for the results.
 table_EB_zigFit_contrasts <- topTable(EB_zigFit_contrasts, coef=1, adjust.method="BH",number = 1000)
 
 
-## We went over how to create basic volcano plots in Lesson 3 Step 3, here we'll
-# use another package, "EnhancedVolcano" which makes for easier plotting.
-if (!requireNamespace('BiocManager', quietly = FALSE))
-  install.packages('BiocManager')
+## We went over how to create basic volcano plots in Lesson 3 Step 3, here we'll go over
+# a few more examples.
 
-BiocManager::install('EnhancedVolcano')
 
-library(EnhancedVolcano)
+## Obtain logical vector regarding whether padj values are less than 0.05
+threshold_OE <- table_EB_zigFit_contrasts$adj.P.Val < 0.05 
+## Determine the number of TRUE values
+length(which(threshold_OE))
+# Add logical vector as a column (threshold) to the res_tableOE
+table_EB_zigFit_contrasts$threshold <- threshold_OE 
+
+## Volcano plot
+# Notice that we use log10 transformation for the pvalues on the y-axis. 
+# This is not necessary, but it can help make the plot easier to read. 
+# Either way, the points will be colored based on the 0.05 value as specified above.
+ggplot(table_EB_zigFit_contrasts) +
+  geom_point(aes(x=logFC, y=-log10(adj.P.Val), colour=threshold)) +
+  xlab("logFC") + 
+  ylab("-log10 adjusted p-value") +
+  #scale_y_continuous(limits = c(0,50)) +
+  theme(legend.position = "none",
+        plot.title = element_text(size = rel(1.5), hjust = 0.5),
+        axis.title = element_text(size = rel(1.25))) 
+  
+# The figure above gives a great overall view of what's going on, but you might
+# also want to figure out the top 5 features (lowest adjusted p-value) in our 
+# differential abundance testing. 
+
+# For this to work, we have to create a new dataframe sorted by adjusted p-value and
+# create a logical vector to indicate which features we want to label.
+
+# As you have come to find out, we'll be needing yet another R package.
+# In this case, we'll use ggrepel which helps with labeling points in ggplot2 figures.
+install.packages(ggrepel)
+library(ggrepel)
+
+## Sort by ordered adj.P.Val
+ordered_table_EB_zigFit_contrasts <- table_EB_zigFit_contrasts[order(table_EB_zigFit_contrasts$adj.P.Val), ] 
+
+## Create a column to indicate which genes to label
+ordered_table_EB_zigFit_contrasts$genelabels <- ""
+# Label only the first 5 genes as TRUE
+ordered_table_EB_zigFit_contrasts$genelabels[1:5] <- TRUE
+
+View(ordered_table_EB_zigFit_contrasts)
+
+# Additionally, we can make the size of each point on the graph correspond with 
+# the "average expression" of each feature (Phyla/AMR gene)
+radius_phylum_node <- sqrt(ordered_table_EB_zigFit_contrasts$AveExpr/pi)
+
+
+# Create plot
+ggplot(ordered_table_EB_zigFit_contrasts) +
+  geom_point(aes(x = logFC, y = -log10(adj.P.Val), colour = threshold, size = radius_phylum_node)) +
+  geom_text_repel(aes(x = logFC, y = -log10(adj.P.Val), label = ifelse(genelabels == TRUE, rownames(ordered_table_EB_zigFit_contrasts),""))) +
+  xlab("logFC") + 
+  ylab("-log10 adjusted p-value") +
+  theme(legend.position = "none",
+        plot.title = element_text(size = rel(1.5), hjust = 0.5),
+        axis.title = element_text(size = rel(1.25))) 
