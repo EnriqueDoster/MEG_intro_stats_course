56a816dce76cb7069ae31a95777bcb6ecb95bd2d
EnriqueDoster <12348747+EnriqueDoster@users.noreply.github.com>
2020-08-05 12:02:00 -0600

    Create install_course_packages.R

---
+++ scripts/install_course_packages.R
@@ -0,0 +1,15 @@
+# Use bioconductor to install packages
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install(c('phyloseq','metagenomeSeq'))
+
+# Install packages from CRAN repository
+install.packages(c("dplyr","ggplot2","data.table","tidyr","forcats", "vegan","plyr", "devtools",
+                   "pheatmap","dendsort","ggrepel","ggpubr","Rtools"))
+
+# Install "pairwiseAdonis" from github for adding pairwise comparisons to models build with the adonis() function
+install_github("pmartinezarbizu/pairwiseAdonis/pairwiseAdonis")
+library(pairwiseAdonis)
+
+# Optional package used for comparing the results from multiple types of differential abundance tests
+# install_github("Russel88/DAtest")
